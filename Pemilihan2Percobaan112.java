f97d02b1627c713f5279d8b1e112f0cacffb769a
Hadhuuu <145408376+Hadhuuu@users.noreply.github.com>
2023-10-22 22:00:50 +0700

    tambahan
    Perubahan

--- Pemilihan2Percobaan112.java
+++ Pemilihan2Percobaan112.java
@@ -10,10 +10,15 @@ public class Pemilihan2Percobaan112 {
         int tahun;
         System.out.println("Masukkan Tahun : ");
         tahun=input12.nextInt();
-        if ((tahun % 4 ) == 0) {
-            if ((tahun % 100) != 0)
-                System.out.println("Tahun Kabisat");
-        } else
-            System.out.println("Bukan tahun kabisat");
+        if ((tahun%400) == 0) {
+            System.out.println("Tahun Kabisat");
+        } else if ((tahun%100)==0) {
+            System.out.println("Bukan Tahun Kabisat");
+        } else if ((tahun%4)==0) {
+            System.out.println("Tahun Kabisat");
+        } else {
+            System.out.println("Bukan Tahun Kabisat");
+        }
+    
     }
 }
